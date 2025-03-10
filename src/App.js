46b8787e499c4a08157660b675a7883812ab001a
SSaha <ssaha@duck.com>
2025-03-10 09:00:35 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://azapi.vsesdns.com/myapi/hello",
+         { mode: 'no-cors'},
+         {headers:  new Headers({
+          'Ocp-Apim-Subscription-Key': '620975c7ee634406a4c4214cd79b2e05',
+          'content-type': 'application/json'
+        })
+        }
+        );
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.status} - ${response.statusText}`);
+        }
+        console.log(response)
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        console.log(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  return (
+    <div>
+      <h1>Fetched Data</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default App;
